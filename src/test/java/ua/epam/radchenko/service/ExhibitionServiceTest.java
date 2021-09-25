fc26f21cf5558dd4be406f9a7b3995b261a094be
Radchenko Vadym <rvskneu@gmail.com>
2021-09-25 05:31:46 +0300

    test

---
+++ src/test/java/ua/epam/radchenko/service/ExhibitionServiceTest.java
@@ -0,0 +1,129 @@
+package ua.epam.radchenko.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+import ua.epam.radchenko.persistence.dao.ExhibitionDao;
+import ua.epam.radchenko.persistence.entity.Exhibition;
+import ua.epam.radchenko.util.type.Status;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+class ExhibitionServiceTest {
+    @InjectMocks
+    private final ExhibitionService exhibitionService = ExhibitionService.getInstance();
+    @Mock
+    private ExhibitionDao exhibitionDao;
+
+    @Test
+    void createExhibitionTest() {
+        int exhibitionId = 1;
+        Exhibition exhibition = Exhibition.newBuilder().build();
+        when(exhibitionDao.insert(exhibition)).then((Answer<Exhibition>) invocationOnMock -> {
+            Exhibition toReturn = invocationOnMock.getArgument(0);
+            toReturn.setExhibitionId(exhibitionId);
+            return toReturn;
+        });
+
+        Exhibition actual = exhibitionService.createExhibition(exhibition);
+
+        assertEquals(exhibitionId, actual.getExhibitionId());
+        verify(exhibitionDao, times(1)).insert(exhibition);
+    }
+
+    @Test
+    void updateExhibitionTest() {
+        Exhibition exhibition = Exhibition.newBuilder().build();
+        exhibitionService.updateExhibition(exhibition);
+        verify(exhibitionDao, times(1)).update(exhibition);
+    }
+
+    @Test
+    void changeStatusOnAnotherStatusTest() {
+        Exhibition exhibition = Exhibition.newBuilder()
+                .setExhibitionStatus(Status.ACTIVE)
+                .build();
+
+        exhibitionService.changeStatus(exhibition, Status.SUSPENDED);
+
+        assertEquals(exhibition.getExhibitionStatus(), Status.SUSPENDED);
+        verify(exhibitionDao, times(1)).update(exhibition);
+    }
+
+    @Test
+    void changeStatusOnSameStatusTest() {
+        Exhibition exhibition = Exhibition.newBuilder()
+                .setExhibitionStatus(Status.ACTIVE)
+                .build();
+
+        exhibitionService.changeStatus(exhibition, Status.ACTIVE);
+
+        assertEquals(exhibition.getExhibitionStatus(), Status.ACTIVE);
+        verify(exhibitionDao, never()).update(exhibition);
+    }
+
+    @Test
+    void findExhibitionByIdWithExistingExhibitionTest() {
+        int exhibitionId = 1;
+        Optional<Exhibition> expected = Optional.of(
+                Exhibition.newBuilder()
+                        .setExhibitionId(exhibitionId)
+                        .build());
+        when(exhibitionDao.findOne(exhibitionId)).thenReturn(expected);
+
+        Optional<Exhibition> actual = exhibitionService.findExhibitionById(exhibitionId);
+
+        assertEquals(expected, actual);
+        verify(exhibitionDao, times(1)).findOne(exhibitionId);
+    }
+
+    @Test
+    void findExhibitionByIdWithNotExistingExhibitionTest() {
+        int exhibitionId = 1;
+        when(exhibitionDao.findOne(exhibitionId)).thenReturn(Optional.empty());
+
+        Optional<Exhibition> exhibitionOpt = exhibitionService.findExhibitionById(exhibitionId);
+
+        assertFalse(exhibitionOpt.isPresent());
+        verify(exhibitionDao, times(1)).findOne(exhibitionId);
+    }
+
+    @Test
+    void findAllExhibitionsTest() {
+        long skip = 0;
+        long limit = 3;
+        List<Exhibition> expected = new ArrayList<Exhibition>() {{
+            add(Exhibition.newBuilder().setExhibitionId(1).build());
+            add(Exhibition.newBuilder().setExhibitionId(2).build());
+            add(Exhibition.newBuilder().setExhibitionId(3).build());
+        }};
+        when(exhibitionDao.findAll(skip, limit)).thenReturn(expected);
+
+        List<Exhibition> actual = exhibitionService.findAllExhibitions(skip, limit);
+
+        assertEquals(3, actual.size());
+        verify(exhibitionDao, times(1)).findAll(skip, limit);
+    }
+
+    @Test
+    void getExhibitionsCountTest() {
+        long expected = 10;
+        when(exhibitionDao.getCount()).thenReturn(expected);
+
+        long actual = exhibitionService.getExhibitionCount();
+
+        assertEquals(expected, actual);
+        verify(exhibitionDao, times(1)).getCount();
+    }
+
+}
---
+++ src/test/java/ua/epam/radchenko/service/ShoppingCartServiceTest.java
@@ -0,0 +1,116 @@
+package ua.epam.radchenko.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ua.epam.radchenko.persistence.entity.Exhibition;
+import ua.epam.radchenko.persistence.entity.Order;
+import ua.epam.radchenko.persistence.entity.User;
+import ua.epam.radchenko.service.entity.ShoppingCart;
+import ua.epam.radchenko.service.exeption.ServiceException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.never;
+
+@ExtendWith(MockitoExtension.class)
+class ShoppingCartServiceTest {
+    @InjectMocks
+    private ShoppingCartService shoppingCartService = ShoppingCartService.getInstance();
+    @Mock
+    private ExhibitionService exhibitionService;
+
+    @Test
+    void addItemToCartTest() {
+        ShoppingCart shoppingCart = mock(ShoppingCart.class);
+        User user = User.newBuilder()
+                .setUserId(1)
+                .build();
+        Exhibition exhibition = Exhibition.newBuilder()
+                .setExhibitionId(1)
+                .build();
+
+        shoppingCartService.addItemToCart(shoppingCart, user, exhibition);
+
+        verify(shoppingCart, times(1)).addItem(any(Order.class));
+    }
+
+    @Test
+    void removeItemFromCartTest() {
+        ShoppingCart shoppingCart = mock(ShoppingCart.class);
+        long cartItemId = 1L;
+
+        shoppingCartService.removeItemFromCart(shoppingCart, cartItemId);
+
+        verify(shoppingCart, times(1)).removeItem(cartItemId);
+    }
+
+    @Test
+    void removeAllItemFromCartTest() {
+        ShoppingCart shoppingCart = mock(ShoppingCart.class);
+
+        shoppingCartService.removeAllItemFromCart(shoppingCart);
+
+        verify(shoppingCart, times(1)).removeAll();
+    }
+
+    @Test
+    void updateShoppingCartItemsFromDatabaseTest() {
+        int exhibitionId = 1;
+        ShoppingCart shoppingCart = mock(ShoppingCart.class);
+        User user = User.newBuilder()
+                .setUserId(1)
+                .build();
+        Exhibition exhibition = Exhibition.newBuilder()
+                .setExhibitionId(exhibitionId)
+                .build();
+        Order item = Order.newBuilder()
+                .setUserId(user)
+                .setExhibitionId(exhibition)
+                .build();
+        List<Order> items = new ArrayList<Order>() {{
+            add(item);
+        }};
+        when(shoppingCart.getItems()).thenReturn(items);
+        when(exhibitionService.findExhibitionById(exhibitionId))
+                .thenReturn(Optional.of(exhibition));
+
+        shoppingCartService.updateShoppingCartItemsFromDatabase(shoppingCart);
+
+        verify(shoppingCart, times(1)).updateItem(item);
+    }
+
+    @Test
+    void updateShoppingCartItemsFromDatabaseWithExceptionTest() {
+        int exhibitionId = 1;
+        ShoppingCart shoppingCart = mock(ShoppingCart.class);
+        User user = User.newBuilder()
+                .setUserId(1)
+                .build();
+        Exhibition exhibition = Exhibition.newBuilder()
+                .setExhibitionId(exhibitionId)
+                .build();
+        Order item = Order.newBuilder()
+                .setUserId(user)
+                .setExhibitionId(exhibition)
+                .build();
+        List<Order> items = new ArrayList<Order>() {{
+            add(item);
+        }};
+        when(shoppingCart.getItems()).thenReturn(items);
+        when(exhibitionService.findExhibitionById(exhibitionId))
+                .thenReturn(Optional.empty());
+
+        ServiceException e = assertThrows(ServiceException.class, () ->
+                shoppingCartService.updateShoppingCartItemsFromDatabase(shoppingCart));
+        assertEquals("Order cannot refer to a non-existent exhibition", e.getMessage());
+        verify(shoppingCart, never()).updateItem(item);
+    }
+}
